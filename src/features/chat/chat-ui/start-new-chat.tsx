28a12901e9dc712146a1019170f30c15e7d6e485
João Vitor Chaves Silva <jvchavessilva@gmail.com>
2023-09-25 13:31:36 -0300

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/start-new-chat.tsx
+++ src/features/chat/chat-ui/start-new-chat.tsx
@@ -19,11 +19,9 @@ export const StartNewChat: FC<Prop> = (props) => {
         </Typography>
         <div className="flex flex-col gap-2">
           <p className="">
-            Welcome to {AI_NAME}. You should interact in a friendly manner with
-            the AI assistant and refrain from participating in any harmful
-            activities.
+            Bem vindo ao {AI_NAME}. Interaja com seu novo assistente baseado em AI de forma amigável.
           </p>
-          <p>You can start a new chat with me by clicking the button below.</p>
+          <p>Clique no botão abaixo para começar uma nova interação.</p>
         </div>
         <div className="-mx-5 -mb-5 p-5 flex flex-col border-t bg-muted">
           <NewChat />
